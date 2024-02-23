bed08378a5dec57e983c2b577e59e926958e4795
Geniuska275 <aigbojie2020@gmail.com>
2024-02-23 12:41:20 +0100

    finally

--- main.js
+++
@@ -1,6 +0,0 @@
-const MySDK = require('./sdk');
-const sdk = new MySDK();
-const iframe = sdk.createIframe('https://www.example.com', 800, 600);
-console.log(iframe)
-// Append the iframe to the document body
-document.body.appendChild(iframe);
