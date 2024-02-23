dfc8c2d89dc8698bf4d42e97d6d9f3212d2ff90c
Geniuska275 <aigbojie2020@gmail.com>
2024-02-23 10:44:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+    });
+});
+
+const PORT =  3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+export function LoadIframe(){
+
+    
+}
